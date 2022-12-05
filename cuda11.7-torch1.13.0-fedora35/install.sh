a7835bd2edaae05855927c8446f5c49a26855c27
Jin Choi <choij@cern.ch>
2022-12-05 14:50:46 +0900

    cuda 11.7

--- cuda11.7-torch1.13.0-fedora35/install.sh
+++ cuda11.7-torch1.13.0-fedora35/install.sh
@@ -3,12 +3,12 @@ source /opt/conda/bin/activate
 conda update -n base -c defaults conda
 # set environment
 conda config --set channel_priority strict
-conda create -n torch -c conda-forge python=3.10 root=6.24.6 -y
+conda create -n torch -c conda-forge python=3.10 root=6.24.06 -y
 conda activate torch
 
 # install torch & torch_geometric
-pip install torch= --extra-index-url https://download.pytorch.org/whl/cu113
-pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.12.1+cu113.html
+pip install torch --extra-index-url https://download.pytorch.org/whl/cu117
+pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.13.0+cu117.html
 
 # install other packages
 conda install numpy pandas matplotlib scikit-learn -c conda-forge -y
